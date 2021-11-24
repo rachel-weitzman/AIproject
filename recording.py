d335519d7df3c5ab778c8057f77b7b699840c03d
malki <malovmalki@gmail.com>
2021-11-24 19:59:22 +0200

    **

--- recording.py
+++ recording.py
@@ -1,15 +1,15 @@
 
 import sounddevice as sd
 from scipy.io.wavfile import write
-import demo
+import speechRecognition
 
 fs = 44100  # Sample rate
-seconds = 1000 # Duration of recording
+seconds = 1000  # Duration of recording
 
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-#myrecording = sd.rec(samplerate=fs, channels=2)
-x=input()
-if x==1:
+# myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+myrecording = sd.rec(samplerate=fs, channels=2)
+x = input()
+if x == 1:
     sd.stop()  # Wait until recording is finished
 write('output.wav', fs, myrecording)  # Save as WAV file
-demo.recognition('output.wav')
+speechRecognition.recognition('output.wav')
